ef260493f6d91f9c09cd5168ea0f1233d2182cf3
Osama <asamtd729@gmail.com>
2026-07-23 23:51:26 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+app = Flask(__name__)
+cred = credentials.Certificate("firebase-key.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    db.collection("users").add({
+        "username": username,
+        "password": password
+    })
+
+    return render_template("dashboard.html")
+
+if "__name__" == "main":
+    app.run(host="0.0.0.0", port=5000)
